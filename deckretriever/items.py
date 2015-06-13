09df2769812875826cfe09c05a95781d229f74c8
JHoloboski <jordan@rewardstyle.com>
2015-06-13 17:47:43 -0500

    deck and event items for the deck_spider

--- deckretriever/items.py
+++ deckretriever/items.py
@@ -9,7 +9,20 @@ import scrapy
 
 
 class EventItem(scrapy.Item):
-    # define the fields for your item here like:
     id = scrapy.Field()
     name = scrapy.Field()
     type = scrapy.Field()
+
+
+class DeckItem(scrapy.Item):
+    name = scrapy.Field()
+    raw_deck = scrapy.Field()  # contents of the deck in a file
+    rank = scrapy.Field()  # what the deck placed in the event
+
+
+class TournamentItem(scrapy.Item):
+    # Would be nice to store this information, decks can relate
+    # back to individual tournaments
+    date = scrapy.Field()
+    format = scrapy.Field()  # which format of standard
+    players = scrapy.Field()  # number of players
