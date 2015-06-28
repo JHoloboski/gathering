22e4524cd2fa602a5e7cf903cf79813f08199d34
JHoloboski <jordan@rewardstyle.com>
2015-06-27 22:57:23 -0500

    adding cards to the db, may need a diff location

--- deck_reader.py
+++ deck_reader.py
@@ -2,43 +2,49 @@
 Reads in a simple mtgo deck file, which is just a txt file delimited by
 newlines
 """
-from deck import Deck
+import requests
+import deckanalyzer.models as models
+from deckanalyzer.models import card
 
 
 class DeckReader(object):
-    def __init__(self, deck_file):
-        self.deck_file = deck_file
+    def __init__(self, deck):
+        self.deck = deck
 
     def __repr__(self):
         return "{0}, {1}".format(
             self.__class__.__name__,
-            self.deck_file
+            self.deck
         )
 
-    def make_deck(self):
+    @staticmethod
+    def get_or_add_card(card_name):
         """
-        Extracts the information from the deck file into a deck dictionary
-        with two primary elements, "main" which is for the main deck, and
-        "side" for the sideboard. We're keeping track of each individual
-        card name and count.
+        Add card to the database if it's not already there
         """
-        with open(self.deck_file, 'r') as deck_file:
-            in_sideboard = False
-            deck = {}
-            deck["main"] = {}
-            deck["side"] = {}
+        with models.session() as session:
+            cards = session.query(
+                card.Card.id
+            ).filter(
+                card.Card.name == card_name
+            ).first()
 
-            for cards in deck_file:
-                if cards.lower().strip() == "sideboard":
-                    in_sideboard = True
-                    continue
+            if cards.count() > 0:
+                return
 
-                count, card_name = cards.split(" ", 1)
-                card_name = card_name.strip()
+            data = {"name": card_name}
+            req = requests.get("http://api.mtgapi.com/v2/cards", params=data)
+            card_info = req.json()
+            card_info = card_info["cards"][0]
 
-                if in_sideboard:
-                    deck["side"][card_name] = count
-                else:
-                    deck["main"][card_name] = count
+            new_card = card.Card(
+                name=card_name,
+                mana_cost=card_info["manaCost"],
+                cmc=int(card_info["cmc"]),
+                types=card_info["types"],
+                text=card_info["text"],
+                power=int(card_info["power"]),
+                toughness=int(card_info["toughness"])
+            )
 
-        return Deck(deck["main"], deck["side"])
+            session.add(new_card)
